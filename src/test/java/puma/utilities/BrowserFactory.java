32cb1276b9ede09435381fe02fff409e2048e243
thilankas961 <thilankas961@gmail.com>
2024-11-06 01:49:36 +0530

    create BrowserFactory class

--- src/test/java/puma/utilities/BrowserFactory.java
+++ src/test/java/puma/utilities/BrowserFactory.java
@@ -1,4 +1,34 @@
 package puma.utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class BrowserFactory {
+    private static WebDriver driver;
+
+    public static WebDriver startBrowser(String browserType) {
+        if (browserType.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browserType.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        } else if (browserType.equalsIgnoreCase("edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        } else {
+            throw new IllegalArgumentException("Browser type not supported: " + browserType);
+        }
+        driver.manage().window().maximize();
+        return driver;
+    }
+
+    public static void quitBrowser() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
